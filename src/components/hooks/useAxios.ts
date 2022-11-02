88832d7b5ce13b7a4ec5b9fe05da52062b100bc5
Robert Rozehnal <rob.kab.roz@gmail.com>
2022-11-02 19:10:44 +0100

    add category

--- src/components/hooks/useAxios.ts
+++ src/components/hooks/useAxios.ts
@@ -11,21 +11,21 @@ const errorInit: ErrorInterface = {
 };
 
 const useAxios = <T extends object>(url: string, n: number = 1) => {
+  const firstRunRef = useRef<boolean>(true);
+
   const [loading, setLoading] = useState<boolean>(false);
   const [data, setData] = useState<T[]>([]);
   const [error, setError] = useState<ErrorInterface>(errorInit);
-
-  const firstRunRef = useRef<boolean>(true);
 
   useEffect(() => {
     const controller = new AbortController();
     const CancelToken = axios.CancelToken;
     const source = CancelToken.source();
 
-    setLoading(true);
-    console.log("firstrun", firstRunRef.current)
+    console.log("firstrun", firstRunRef.current);
 
-    const getData = async () => {      
+    const getData = async () => {
+      setLoading(true);
       console.log("In getData function");
       for (let i = 1; i <= n; i++) {
         try {
@@ -34,43 +34,26 @@ const useAxios = <T extends object>(url: string, n: number = 1) => {
             cancelToken: source.token,
             signal: controller.signal,
           });
-          setData((prev) => [...prev, ...response.data.results]);          
+          setData((prev) => [...prev, ...response.data.results]);
           setLoading(false);
         } catch (err) {
-          let message: string;
+          
           const errors = err as Error | AxiosError;
-
-          if (axios.isCancel(errors)) {
-            setError({ errorMessage: errors.message, isError: true });
+          if (errors.message === "canceled"){
+            return;
           }
-
-          if (axios.isAxiosError(err)) {
-            if (err.code) {
-              console.log(err);
-              message = String(err);
-              setError({ errorMessage: message, isError: true });
-            }
-          } else {
-            console.log("normal errors, not axios error");
-            message = "normal error";
-            setError({ errorMessage: message, isError: true });
-          }
+          console.log("normal errors, not axios error", errors);
+          setError({ errorMessage: String(errors), isError: true });
         }
       }
-      
     };
 
-    if (!firstRunRef.current) {
-      getData();   
-      console.log("After GetData", firstRunRef.current);   
-    }
-    
+    getData();
 
-    return () => {      
+    return () => {
       controller.abort();
       firstRunRef.current = false;
       console.log("useEffect cleaning", firstRunRef.current);
-      
     };
   }, [url, n]);
 
